31d4e8ac59f03c132453694a1d1c2935d2e49ae7
AndyLampert <lamperta@gmail.com>
2014-04-15 15:52:37 -0600

    finished exercise 2 and added jasmine testing

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ var tripleFive = function() {
 // function call
 tripleFive();
 
+
+var lastLetter = function(str) {
+    var myString = "hello";
+    var stringLength = myString.length; 
+    var lastChar = myString.charAt(stringLength - 1); 
+    return lastChar;
+}
+
+console.log(lastLetter('hello'));
+console.log(lastLetter('island'));
+
+
---
+++ test/spec.js
@@ -0,0 +1,11 @@
+// describe(tripleFive, tripleFive(){
+//   it("should console.log \"Five!\" 3 times", tripleFive() {
+//     expect(tripleFive()).toEqual("Five!");
+//   });
+// });
+
+describe("get last letter", function() {
+  it("should take the last letter of a string", function(){
+    expect(lastLetter("hello")).toEqual("o");
+  });
+});
